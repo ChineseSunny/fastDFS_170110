1dd2cef6f007ff614922ad8424d19ab980cb611c
ChineseSunny <chinasunny@aliyun.com>
2017-05-19 22:22:03 +0800

    src commit

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <pthread.h>
+
+#include "fdfs_api.h"
+#include "make_log.h"
+
+#define FDFS_MAIN "main"
+#define FDFS_MAIN_PROC "fdfs_api"
+
+int main(int argc,char *argv[])
+{
+	int ret = 0;
+	char file_id[128] = { 0 };
+	
+	ret = myupload_byexec("a.c",file_id);
+	if(ret)
+	{
+		printf("myupload_byexec err\n");
+		return ret;
+	}
+	
+	LOG(FDFS_MAIN,FDFS_MAIN_PROC,"%s",file_id);
+	//printf("file_id = [%s]\n",file_id);
+	
+	return 0;
+}
---
+++ src/start.sh
@@ -0,0 +1,5 @@
+sudo fdfs_trackerd /etc/fdfs/tracker.conf
+
+sudo fdfs_storaged /etc/fdfs/storage.conf
+
+
